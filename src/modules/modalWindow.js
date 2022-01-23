d8c88ceceecc0daf5d1f171124b2ea881b19819c
Anna Zolotareva <anazolotarewa@yandex.ru>
2022-01-23 14:07:33 +0400

    изменения в модуле modalWindow.js

--- src/modules/modalWindow.js
+++ src/modules/modalWindow.js
@@ -1,25 +1,34 @@
 const modalWindow = () => {
-    const callbackBtns  = document.querySelectorAll('.callback-btn');
-    const servicesbtn = document.querySelector('.button-services');
     const modal = document.querySelector('.modal-callback');
     const modalOverlay = document.querySelector('.modal-overlay');
     const modalCloseBtn = document.querySelector('.modal-close');
     const elementsOfCarousel = document.querySelectorAll('.services-carousel .fancyboxModal');
     const imgWrappers = document.querySelectorAll('.services-carousel .img-wrapper');
+    const mainWrapper = document.querySelector(".main-wrapper");
+
+    const visibleModalWindow = () => {
+        modal.style.display = 'block';
+        modalOverlay.style.display = 'block';  
+    };
+
+    const unvisibleModalWindow = () => {
+        modal.style.display = 'none';
+        modalOverlay.style.display = 'none';
+    };
 
     const openModalWindow = (button) => {
         button.addEventListener('click', (e) => {
             e.preventDefault();
-            modal.style.display = 'block';
-            modalOverlay.style.display = 'block';   
+            if (e.target.matches(".services-carousel .img-wrapper") || e.target.matches(".callback-btn") || e.target.classList.contains("button-services")) {
+                visibleModalWindow();
+            }   
         });
     };
 
     const closeModalWindow = (elem) => {
         elem.addEventListener("click", (e) => {
             e.preventDefault();
-            modal.style.display = 'none';
-            modalOverlay.style.display = 'none';
+            unvisibleModalWindow();
         });
     };
 
@@ -37,17 +46,7 @@ const modalWindow = () => {
 
     removeClassAbsolute();
     addClassAbsolute();
-    
-    imgWrappers.forEach((wrapper) => {
-        addClassAbsolute();
-        openModalWindow(wrapper);
-    });
-
-    callbackBtns.forEach((btn) => {
-        openModalWindow(btn);
-    });
-
-    openModalWindow(servicesbtn);
+    openModalWindow(mainWrapper);
     closeModalWindow(modalOverlay);
     closeModalWindow(modalCloseBtn);
 
